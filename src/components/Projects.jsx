89915b2ca256bec05693d12d97e87d073b948769
JeremyB60 <ballin.jeremy@gmail.com>
2024-01-07 23:04:08 +0100

    Mise à jour d'une couleur

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -255,7 +255,7 @@ const Projects = () => {
                             viewBox="0 -960 960 960"
                             width="24"
                             className="mr-2"
-                            fill="#00b700"
+                            fill="#6de05d"
                           >
                             <path d="m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z" />
                           </svg>
